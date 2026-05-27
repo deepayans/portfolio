4ee2bd4b350096008c2259abca771070daf5662b
SARKAR Deepayan <deepayans77@gmail.com>
2026-05-28 04:18:46 +0530

    Update education period to 'Present'

--- data/resume.ts
+++ data/resume.ts
@@ -53,7 +53,7 @@ export const resume = {
       school: "KEDGE Business School",
       degree: "Master of Science in Data Analytics for Business",
       level: "Master 2nd Year",
-      period: "Sep 2025 — Apr 2027",
+      period: "Sep 2025 — Present",
       location: "Bordeaux, France",
       current: true,
     },
